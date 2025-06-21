9ff4b8d9690df5b40fb0144da8abb84128cff463
kishore kumar C R <76273394+Reddy07021998@users.noreply.github.com>
2025-06-21 22:36:47 +0530

    push

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -314,7 +314,7 @@ elif st.session_state.current_screen == "register":
 
 # Main Menu Screen
 elif st.session_state.current_screen == "main_menu":
-    st.Title("Expense Tracker Dashboard")
+    st.title("Expense Tracker Dashboard")
 
     categories_df = run_async(fetch_categories())
 
